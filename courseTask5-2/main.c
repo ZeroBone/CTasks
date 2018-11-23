7aee87a8d4c2ec5448fbf8229fdd6c4e83a53ed8
Alexander <zerobone21@gmail.com>
2018-11-23 19:36:31 +0400

    5-2 tests

--- courseTask5-2/main.c
+++ courseTask5-2/main.c
@@ -8,6 +8,63 @@ void processMatrix(double* matrix, unsigned int sizeY, unsigned int sizeX);
 void outputMatrix(double* matrix, unsigned int y, unsigned int x);
 int positiveElements(double* row, unsigned int rowLength);
 
+/*
+Matrix B:
+    1.00    2.00    3.00
+    4.00    5.00    6.00
+    7.00    8.00    9.00
+
+Line 1 doesn't contain 2 positive values.
+Line 2 doesn't contain 2 positive values.
+Line 3 doesn't contain 2 positive values.
+Matrix C:
+   -1.00    2.00    3.00
+    1.00   -2.00    3.00
+    1.00    2.00   -3.00
+
+Line 1 contains exactly 2 positive values.
+Line 2 contains exactly 2 positive values.
+Line 3 contains exactly 2 positive values.
+*/
+
+/*
+Matrix B:
+    0.00   11.00    3.00
+    6.00   98.00    2.00
+   84.00   49.00   10.00
+
+Line 1 contains exactly 2 positive values.
+Line 2 doesn't contain 2 positive values.
+Line 3 doesn't contain 2 positive values.
+Matrix C:
+   12.00   13.00   32.00
+   41.00   65.00   35.00
+   -9.00    1.00    0.00
+
+Line 1 doesn't contain 2 positive values.
+Line 2 doesn't contain 2 positive values.
+Line 3 doesn't contain 2 positive values.
+*/
+
+/*
+Matrix B:
+    1.00    0.00   -1.00
+   -0.00   -0.00   -0.00
+ 1066.00   -5.00  346.00
+
+Line 1 doesn't contain 2 positive values.
+Line 2 doesn't contain 2 positive values.
+Line 3 contains exactly 2 positive values.
+Matrix C:
+    0.10    0.38   -0.20
+    6.00    9.50  -64.00
+   14.00    8.00  -45.00
+
+Line 1 contains exactly 2 positive values.
+Line 2 contains exactly 2 positive values.
+Line 3 contains exactly 2 positive values.
+*/
+
 int main() {
 	
 	double matrixB[MATRIX_M][MATRIX_N];
