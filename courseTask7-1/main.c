e96f971b9ff52cdaa7da4d385246d16a529773bd
Alexander <zerobone21@gmail.com>
2018-12-03 21:05:40 +0400

    Completed 7-1 task

--- courseTask7-1/main.c
+++ courseTask7-1/main.c
@@ -2,53 +2,141 @@
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-#define MAX_NAME_LENGTH 50
+#include <windows.h>
+#define MAX_FNAME_LENGTH 50
+
+char phraseOk(char*);
 
 int main(int argc, char *argv[]) {
 	
 	setlocale(LC_ALL, "Russian");
 	
-	char *inputFile[MAX_NAME_LENGTH];
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+	char inputFileName[MAX_FNAME_LENGTH];
 	
 	if (argc < 2) {
 		
-		printf("Insifficient amount of args specified.\n");
-		
 		printf("Enter input file name:\n");
-		fgets(inputFile, MAX_NAME_LENGTH, stdin);
+		
+		fgets(inputFileName, MAX_FNAME_LENGTH, stdin);
 		
 	}
 	else {
 		
-		strcpy(inputFile, argv[1]);
+		strcpy(inputFileName, argv[1]);
 		
 	}
 	
 	printf("Input file: ");
-	puts(inputFile);
+	puts(inputFileName);
 	
-	FILE *file = fopen(inputFile, "r+");
+	FILE *inputFile, *tempFile;
 	
-	int current;
-	char* currentPhrase[50];
-	
-	/*while ((current = fgetc(file)) != EOF) {
+	if ((inputFile = fopen(inputFileName, "r")) == NULL) {
 		
-		printf("%c", current);
+		perror("An error occurred while trying to read the file");
 		
+		system("pause");
 		
-		
-	}*/
-	
-	while (fscanf(file, )) {
-		
-		
+		return 1;
 		
 	}
 	
-	fclose(file);
+	if ((tempFile = fopen("temp.txt", "w")) == NULL) {
+		
+		perror("An error occurred while trying to create a temp file");
+		
+		system("pause");
+		
+		return 1;
+		
+	}
+	
+	int currentSymbol, phraseStartPosition, currentPosition, temp, phraseLength;
+	
+	phraseStartPosition = 0; /* ftell(inputFile); */
+	
+	/* fscanf(inputFile, "%d", &cur) != EOF */
+	while (1) {
+		
+		currentSymbol = fgetc(inputFile);
+		
+		if (currentSymbol == ' ') continue;
+		
+		if (currentSymbol == '.' || currentSymbol == EOF) {
+			
+			/* current pharase has ended */
+			
+			currentPosition = ftell(inputFile);
+			
+			fseek(inputFile, phraseStartPosition, SEEK_SET);
+			
+			phraseLength = currentPosition - phraseStartPosition;
+			
+			char* phrase = (char*)malloc(phraseLength * sizeof(char) + sizeof(char)); /* +1 for null-terminator */
+			
+			phrase[phraseLength - 1] = '\0';
+			
+			fread(phrase, sizeof(char), phraseLength, inputFile);
+			
+			printf("Current phrase (%d): ", phrase[0]);
+			puts(phrase);
+			
+			if (phraseOk(phrase)) {
+				
+				fwrite(phrase, sizeof(char), phraseLength, tempFile);
+				
+			}
+			
+			if (currentSymbol == EOF) break;
+			
+			fseek(inputFile, currentPosition, SEEK_SET);
+			
+			phraseStartPosition = currentPosition + 1;
+			
+			/*do {
+				
+				temp = fgetc(inputFile);
+				
+				currentPosition++;
+				
+			} while (temp != '\n' && temp != EOF);
+			
+			phraseStartPosition = currentPosition + 1;
+			
+			fseek(inputFile, phraseStartPosition, SEEK_SET);*/
+			
+			
+		}
+		
+	}
+	
+	fclose(inputFile);
+	fclose(tempFile);
 	
 	system("pause");
+	
+	return 0;
+	
+}
+
+char phraseOk(char* phrase) {
+	
+	int d;
+	
+	if (sscanf(phrase, "%d", &d) || sscanf(phrase, "%d-é", &d)) return 1;
+	
+	while (*phrase++ != '\0') {
+		
+		if (*phrase == ' ') {
+			
+			if (sscanf(phrase, "%d", &d) || sscanf(phrase, "%d-é", &d)) return 1;
+			
+		}
+		
+	}
 	
 	return 0;
 	
