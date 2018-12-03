f174c30bd0f6d6ccb76fd5095b95e7aba5b7ab6d
Alexander <zerobone21@gmail.com>
2018-12-03 23:20:29 +0400

    Invalid argument bug fixed. fgets leaves \n in the string.

--- courseTask7-2/main.c
+++ courseTask7-2/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <windows.h>
+#include <math.h>
 #define MAX_FNAME_LENGTH 100
 
 int main() {
@@ -19,8 +20,9 @@ int main() {
 	puts("Enter input file name:");
 	
 	fgets(inputFileName, MAX_FNAME_LENGTH, stdin);
+	inputFileName[strlen(inputFileName) - 1] = '\0';
 	
-	printf("Input file name: ");
+	printf("Input file name:");
 	puts(inputFileName);
 	
 	if ((inputFile = fopen(inputFileName, "r")) == NULL) {
@@ -45,13 +47,43 @@ int main() {
 	
 	/* file streams opened */
 	
-	int matrixElement;
+	int matrixElement, count = 0;
 	
 	while (fscanf(inputFile, "%d", &matrixElement) != EOF) {
 		
 		printf("Current matrix element: %d\n", matrixElement);
 		
+		count++;
+		
 	}
+	
+	count = sqrt(count);
+	
+	printf("This is a %dx%d matrix.\n", count, count);
+	
+	int x, y;
+	
+	for (y = 0; y < count; y++) {
+		
+		for (x = 0; x < count; x++) {
+			
+			if (x == count - 1) {
+				
+				fprintf(outputFile, "%.2f\n", 1.0);
+				
+			}
+			else {
+				
+				fprintf(outputFile, "%.2f ", 1.19);
+				
+			}
+			
+		}
+		
+	}
+	
+	fclose(inputFile);
+	fclose(outputFile);
 	
 	system("pause");
 	
